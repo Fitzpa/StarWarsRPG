c20e38736d79e8457aca9d77676d0c19f8e1e4ec
Louie Fitzpatrick <fitz4492@gmail.com>
2019-02-21 15:20:35 -0600

    most of the html bootstrap and js

---
+++ assets/js/game.js
@@ -0,0 +1,35 @@
+
+var characterList = [
+  {
+    name: "Obi Wan Kenobi",
+    health: 120,
+    attack: 8,
+    counter: 15,
+    dfltAttak: 8,
+    pic: "assets/images/obi-wan-kenobi.jpg"
+  }
+  {
+    name: "Luke Skywalker",
+    health: 100,
+    attack: 15,
+    counter: 25,
+    dfltAttak: 15,
+    pic: "assets/images/Luke-Skywalker.jpeg"
+  }
+  {
+    name: "Darth Vader",
+    health: 150,
+    attack: 12,
+    counter: 15,
+    dfltAttak: 12,
+    pic: "assets/images/Darth-Vader.jpeg"
+  }
+  {
+    name: "Darth Maul",
+    health: 180,
+    attack: 14,
+    counter: 18,
+    dfltAttak: 14,
+    pic: "assets/images/Darth-Maul.jpeg"
+  }
+]
